55b1e6bfa9283d0416ac7a8b2bffa930a1e45808
Sky Immortal <ojeremiah721@gmail.com>
2024-11-14 21:23:55 +0100

    Updated_Ultra

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-
+// src/App.js
 import { useEffect } from 'react';
 import './App.css';
 import Navbar from './components/navbar.js';
 import Hero from './components/hero.js';
 import About from './components/about.js';
+import ScrollToTopButton from './components/scrolltoTop.js';
+import Ovals from './components/Ovals'; // Import the Ovals component
 import AOS from 'aos';
 
 const App = () => {
-
   useEffect(() => {
-
+    // Initialize AOS (Animate On Scroll)
     setTimeout(() => {
-
       AOS.init();
-
     }, 1000);
-
-  }, []);
+  }, []); // Run once on mount
 
   return (
     <div className='scroll'>
-
+      <Ovals /> {/* Render the Ovals component */}
+      <Navbar />
       <Hero />
       <About />
-      <Navbar />
-      
+      <ScrollToTopButton />
     </div>
-    
-
-  )
+  );
 }
 
 export default App;
